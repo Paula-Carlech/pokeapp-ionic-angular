3ad49d595242995ccedcc04ad1003c137517a62a
Paula Carlech <paulacarlech@gmail.com>
2025-06-16 17:24:36 -0300

    feat: Add component setup
    Set up main components with necessary imports and lifecycle
    hooks to handle data and user interactions.

--- pokeapp/src/app/app.component.ts
+++ pokeapp/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  standalone: true,
+  imports: [IonicModule, RouterModule],
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
-  constructor() {}
-}
+export class AppComponent { }
