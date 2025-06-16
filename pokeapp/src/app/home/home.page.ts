d556bec790a23001c8c46d3eb392be4c1d5066f9
Paula Carlech <paulacarlech@gmail.com>
2025-06-16 17:22:49 -0300

    feat: Add home page
    Implemented the main home page displaying a list of Pokémons
    with pagination and navigation to the details page.

--- pokeapp/src/app/home/home.page.ts
+++ pokeapp/src/app/home/home.page.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { PokemonService } from '../services/pokemon.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterModule],
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  pokemons: any[] = [];
+  offset = 0;
+
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnInit() {
+    this.loadPokemons();
+  }
+
+  // Load Pokémon with pagination
+  loadPokemons() {
+    this.pokemonService.getPokemons(20, this.offset).subscribe((data) => {
+      data.results.forEach((p: any) => {
+        const id = p.url.split('/')[6];
+        this.pokemons.push({
+          name: p.name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+          id: id
+        });
+      });
+    });
+  }
+
+  loadMore() {
+    this.offset += 20;
+    this.loadPokemons();
+  }
 }
